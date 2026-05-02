ad2bd894e03ed8139d6f00a0bde3afe95113f60a
AlexandrSedov <sed0ff4lex@yandex.ru>
2026-05-02 17:27:52 +0300

    added routes

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', static fn () => redirect()->to('/comments'));
+
+use App\Controllers\Comments;
+
+$routes->get('comments', [Comments::class, 'index']);
+$routes->post('comments', [Comments::class, 'create']);
+$routes->post('comments/delete/(:num)', [Comments::class, 'delete']);
